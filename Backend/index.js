3645f134364c5155d991df0ea7066033c069990f
Prem Kumar Yadav <premnsa108@gmail.com>
2025-06-24 14:11:21 +0545

    2025

--- Backend/index.js
+++ Backend/index.js
@@ -29,6 +29,8 @@ const __dirname = path.dirname(__filename);
 const hardOrigins = [
     'http://202.45.146.226',
     'http://202.45.146.226:5173',
+    'https://202.45.146.226',
+    'https://202.45.146.226:5173',
     'http://10.5.60.151',
     'http://10.5.60.151:5173',
     'http://10.5.60.151:5174',
