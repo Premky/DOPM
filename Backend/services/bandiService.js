f7271911edf96a662f8185550b4a8a554a3b8500
Prem Kumar Yadav <www.prem.mass@gmail.com>
2025-07-07 13:23:23 +0545

    1.23

--- Backend/services/bandiService.js
+++ Backend/services/bandiService.js
@@ -260,13 +260,13 @@ async function insertSingleFineDetails( bandi_id, data, user_id, office_id ) {
       console.log(values)
     } else {
       sql = `INSERT INTO bandi_fine_details (
-        bandi_id, fine_type_id, amount_fixed,
+        bandi_id, amount_fixed,
         created_by, updated_by, current_office_id
       ) VALUES (?)`;
 
       values = [
         bandi_id,
-        data.fine_type_id,
+        
         data.is_fine_fixed,
         user_id,
         user_id,
