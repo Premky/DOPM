b8583639a7c03a18e2941f9ca2a0018dd865a3a3
Prem Kumar Yadav <www.prem.mass@gmail.com>
2025-08-11 13:41:19 +0545

    1.41

--- Backend/routes/payroleRoute.js
+++ Backend/routes/payroleRoute.js
@@ -1212,7 +1212,7 @@ router.get( '/get_selected_mudda_group/:id', verifyToken, async ( req, res ) => 
 } );
 
 router.get( '/get_bandi_mudda/', async ( req, res ) => {
-    // const active_office = req.user.office_id;
+    
     const { id } = req.params;
     const sql = `
         SELECT bmd.*, m.mudda_name,
