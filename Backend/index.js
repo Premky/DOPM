21f685019fbf51d65a40917f0ccc3aa5ddad36d1
Prem Kumar Yadav <www.prem.mass@gmail.com>
2025-08-31 16:35:24 +0545

    4.33

--- Backend/index.js
+++ Backend/index.js
@@ -105,16 +105,16 @@ app.use(cors({
 }));
 
 
-app.options( '*', ( req, res ) => {
-  const origin = req.headers.origin;
-  if ( allowedOrigins.includes( origin ) ) {
-    res.header( "Access-Control-Allow-Origin", origin );
-    res.header( "Access-Control-Allow-Credentials", "true" );
-  }
-  res.header( "Access-Control-Allow-Headers", "Content-Type, Authorization, Cache-Control, Pragma, Expires" );
-  res.header( "Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS" );
-  res.sendStatus( 204 );
-} );
+// app.options( '*', ( req, res ) => {
+//   const origin = req.headers.origin;
+//   if ( allowedOrigins.includes( origin ) ) {
+//     res.header( "Access-Control-Allow-Origin", origin );
+//     res.header( "Access-Control-Allow-Credentials", "true" );
+//   }
+//   res.header( "Access-Control-Allow-Headers", "Content-Type, Authorization, Cache-Control, Pragma, Expires" );
+//   res.header( "Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS" );
+//   res.sendStatus( 204 );
+// } );
 
 
 // ------------------- 3️⃣ Security Headers -------------------
