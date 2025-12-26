1fae8e672213108eb922979ad20bebcdcbac5fb9
Prem Kumar Yadav <www.prem.mass@gmail.com>
2025-12-26 13:54:39 +0545

    1.54

--- Backend/services/bandiStatusService.js
+++ Backend/services/bandiStatusService.js
@@ -8,17 +8,31 @@ export async function updateBandiStatus({
   decisionDate,
   userId,
 }) {
-  await pool.query(
-    `UPDATE bandi_person
-     SET bandi_status = ?, status_updated_at =?, status_updated_by = ?
-     WHERE id = ?`,
-    [newStatusId, new Date(), userId, bandiId]
-  );
+  const conn = await pool.getConnection();
+  try{
+    await conn.beginTransaction();
 
-  await pool.query(
-    `INSERT INTO bandi_status_history
-     (bandi_id, status_code, source_table, remarks, decision_date, created_by)
-     VALUES (?, ?, ?, ?, ?, ?)`,
-    [bandiId, historyCode, source, remarks, decisionDate, userId]
-  );
+    await conn.query(
+      `UPDATE bandi_person
+       SET bandi_status = ?, status_updated_at =?, status_updated_by = ?
+       WHERE id = ?`,
+      [newStatusId, new Date(), userId, bandiId]
+    );
+  
+    await conn.query(
+      `INSERT INTO bandi_status_history
+       (bandi_id, status_code, source_table, remarks, decision_date, created_by)
+       VALUES (?, ?, ?, ?, ?, ?)`,
+      [bandiId, historyCode, source, remarks, decisionDate, userId]
+    );
+
+    await conn.commit();
+
+  } catch(error){
+    await conn.rollback();
+    console.error("Transaction failed:", error);
+    throw error;
+  } finally{
+    conn.release();
+  }
 }
