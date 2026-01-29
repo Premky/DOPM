c3e3dc2e5c36141be62fa1bd7bf893a02e6bed10
Prem Kumar Yadav <www.prem.mass@gmail.com>
2026-01-29 14:07:32 +0545

    2.07

--- Frontend/src/Components/Parole/Tables/TableActions/OfficeAdminActions.jsx
+++ Frontend/src/Components/Parole/Tables/TableActions/OfficeAdminActions.jsx
@@ -4,13 +4,13 @@ const OfficeAdminActions = ( { status, onForward, onApprove } ) => {
   return (
     <>
       {/* Forward to next level */}
-      {payrole_status < 16 && (
+      {status < 16 && (
         <MenuItem onClick={onForward}>
           कारागार प्रशासकबाट अगाडि पठाउनुहोस्
         </MenuItem>
       )}
 
-      {(payrole_status === 16 || payrole_status===17) && (
+      {(status === 16 || status===17) && (
         <MenuItem onClick={onApprove}>
           अदालतको निर्णय राख्नुहोस्
         </MenuItem>
