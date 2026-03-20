17d0eb8619641609b047dbc6db5361811125bf75
Prem Kumar Yadav <www.prem.mass@gmail.com>
2026-03-20 22:02:21 +0545

    styles updated

---
+++ Frontend/src/styles/tableheadStyle.jsx
@@ -0,0 +1,36 @@
+export const tableHeadStyle = {
+    background: "teal",
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: "0.9 rem",
+    textAlign: "center",
+    border: "1px solid #ddd",
+    margin: 0,
+    padding: "2px"
+} ;
+
+export const tableHead2ndRowStyle =  {
+    background: "teal",
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: "0.9 rem",
+    textAlign: "center",
+    border: "1px solid #ddd",
+    margin: 0,
+    padding: "2px",
+    position: 'sticky',
+    top: 51, 
+    zIndex: 1
+} ;
+
+export const tableTotalRowStyle =  {
+    background: "#f0f0f0",
+    color: "#000000",
+    fontWeight: "bold",
+    fontSize: "0.9 rem",
+    textAlign: "center",
+    // border: "1px solid #ddd",
+    margin: 0,
+    padding: "2px"
+} ;
+
