c8c38a6d6f619129e86b1002cccf18c766a42caf
Prem Kumar Yadav <www.prem.mass@gmail.com>
2026-03-21 22:49:04 +0545

    10.49

--- Frontend/src/styles/tableheadStyle.jsx
+++ Frontend/src/styles/tableheadStyle.jsx
@@ -6,10 +6,10 @@ export const tableHeadStyle = {
     textAlign: "center",
     border: "1px solid #ddd",
     margin: 0,
-    padding: "2px"
-} ;
+    padding: "1px"
+};
 
-export const tableHead2ndRowStyle =  {
+export const tableHead2ndRowStyle = {
     background: "teal",
     color: "#ffffff",
     fontWeight: "bold",
@@ -19,11 +19,11 @@ export const tableHead2ndRowStyle =  {
     margin: 0,
     padding: "2px",
     position: 'sticky',
-    top: 30, 
+    top: 30,
     zIndex: 1
-} ;
+};
 
-export const tableHead3rdRowStyle =  {
+export const tableHead3rdRowStyle = {
     background: "teal",
     color: "#ffffff",
     fontWeight: "bold",
@@ -33,18 +33,28 @@ export const tableHead3rdRowStyle =  {
     margin: 0,
     padding: "2px",
     position: 'sticky',
-    top: 60, 
+    top: 90,
     zIndex: 2
-} ;
+};
 
 export const tableBodyRowStyle = {
     background: "#f8f5f5",
     // color: "#ffffff",
     margin: 1,
     padding: "5px"
-} ;
+};
 
-export const tableTotalRowStyle =  {
+export const tableBodyRowVerticalTextStyle = {
+    background: "#f8f5f5",
+    // color: "#ffffff",
+    margin: 1,
+    padding: "5px",
+    transform: "rotate(-90deg)",
+    whiteSpace: "nowrap",
+    display: "inline-block"
+};
+
+export const tableTotalRowStyle = {
     background: "#f0f0f0",
     color: "#000000",
     fontWeight: "bold",
@@ -53,5 +63,5 @@ export const tableTotalRowStyle =  {
     // border: "1px solid #ddd",
     margin: 0,
     padding: "2px"
-} ;
+};
 
